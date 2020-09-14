32ac33634b75e057d91d5e6ad2e56ebbbecdfa99
Ben <alphanumu@gmail.com>
2020-09-14 17:17:20 +0100

    o

--- gfx/video_crt_switch.c
+++ gfx/video_crt_switch.c
@@ -518,7 +518,7 @@ static void crt_kms_switch(unsigned width, unsigned height,
 
 	dmode.type        = DRM_MODE_TYPE_USERDEF;	//DRM_MODE_TYPE_DRIVER | DRM_MODE_TYPE_PREFERRED;
 
-   drmModeModeInfo *mode = {};
+   drmModeModeInfo *mode = &dmode;
 
 	mode->type |= CUSTOM_VIDEO_TIMING_DRMKMS;
 
